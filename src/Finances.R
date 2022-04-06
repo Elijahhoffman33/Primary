871f4399bb6177b16aee826dcfd5901e7edfc7fc
Elijah Hoffman <elijahhoffman33@gmail.com>
2022-04-05 20:00:55 -0700

    Finances Update

--- src/Finances.R
+++ src/Finances.R
@@ -26,8 +26,49 @@ effective_rate = (actual_check*2)/Prorated
 print(paste0('Effective tax / benefits rate: ',
         round(1-effective_rate,3) ))
 
+period_total = 3846.15
+#Fed tax %
+fed = 568 / period_total
+fed
+#State tax %
+state = 247.74 / period_total
+state
+#social security / medicare / disability
+ss_other = (238.46 + 55.77 + 42.31) / period_total
+ss_other
+fed + state + ss_other
+
+### Catch Up 
+total_catch_up = 8680.91
+fed_catch_up = 1858.18 / total_catch_up
+fed_catch_up
+#State tax %
+state_catch_up = 742.33 / total_catch_up
+state_catch_up
+#social security / medicare / disability
+ss_other_catch_up = (538.22 + 125.88 + 95.49) / total_catch_up
+ss_other_catch_up
+fed_catch_up + state_catch_up + ss_other_catch_up
+
+### differences
+reduce_fed = fed_catch_up - fed 
+reduce_fed
+reduce_state = state_catch_up - state
+reduce_state
+(ss_other_catch_up - ss_other) %>% round(3)
+
+
+### adjustments
+adj_fed = round((fed_catch_up - reduce_fed) * total_catch_up,2) 
+adj_state = round((state_catch_up - reduce_state) * total_catch_up,2)
+
+print(paste0('Reduce federal tax to: ',
+      adj_fed ))
+print(paste0('Reduce federal tax to: ',
+              adj_state ))
+
 # self scratch
-1152.28 / 3846.15 
+1152.28 / period_total 
 
 3360.10 / 8680.91 
 
